cd261ea93c942834e789d2ee9065a315f31a6705
Karume-lab <danielkarume@outlook.com>
2023-02-04 20:16:36 +0300

    Rectify a looping linked list

--- Linked List/removeLoop.cpp
+++ Linked List/removeLoop.cpp
@@ -2,15 +2,21 @@
 
 int removeLoop(student_t **head)
 {    
-    student_t *slowPtr = (*head);
     student_t *fastPtr = findLoop(head);
+    student_t *slowPtr = (*head);
 
+    if (fastPtr->next == *head)
+    {
+        fastPtr->next = NULL;
+        return (0);
+    }
+    
     while (slowPtr->next != fastPtr->next)
     {
         slowPtr = slowPtr->next;
         fastPtr = fastPtr->next;
     }
+
     fastPtr->next = NULL;
-    
     return (0);
 }
