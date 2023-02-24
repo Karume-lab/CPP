d3be9048009c8e8371dabe41bb302a0c34b5724d
Karume-lab <danielkarume@outlook.com>
2023-02-24 13:07:50 +0300

    Burble Sort

--- Algorithms/SORTING/burbleSort.cpp
+++ Algorithms/SORTING/burbleSort.cpp
@@ -2,7 +2,7 @@ int burbleSort(int size, int arr[])
 {
     for (int i = 0; i < size; i++)
         {
-            for (int j = i; j < size; j++)
+            for (int j = i+1; j < size; j++)
             {
                 if (arr[i] > arr[j])
                 {
