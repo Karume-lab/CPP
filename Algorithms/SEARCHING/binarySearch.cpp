008e8e0ca150f4e1b45e68f863429cb814080c0b
Karume-lab <danielkarume@outlook.com>
2023-02-24 15:04:31 +0300

    Binary Search

--- Algorithms/SEARCHING/binarySearch.cpp
+++ Algorithms/SEARCHING/binarySearch.cpp
@@ -1,4 +1,4 @@
-int binarySearch(int key, int array[], int size, int left, int right)
+int binarySearchRec(int key, int array[], int size, int left, int right)
 {   
 
     while (left <= right)
@@ -10,14 +10,35 @@ int binarySearch(int key, int array[], int size, int left, int right)
         else if (key > array[mid])
         {
             left = mid +1;
-            return (binarySearch(key, array, size, left, right));
+            return (binarySearchRec(key, array, size, left, right));
         }        
         else
         {
             right = mid -1;
-            return (binarySearch(key, array, size, left, right));
+            return (binarySearchRec(key, array, size, left, right));
         }
     }
+
+    return (-1);
+}
+
+int binarySearchIter(int key, int arr[], int size, int left, int right)
+{
+    int mid;
+    
+    while (left <= right)
+    {
+        mid = left + ((right - left)/2);
+        if (key == arr[mid])
+            return (mid);
+        else if (key > arr[mid])
+        {
+            left = mid +1;
+        }
+        else
+            right = mid -1;
+    }
+    
 
     return (-1);
 }
